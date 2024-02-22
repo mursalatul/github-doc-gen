de9089f0a6c868be32269d3479303fca2165ce29
Md. Mursalatul Islam Pallob <mursalatulislampallob@gmail.com>
2024-02-23 00:12:02 +0600

    Update UPDATE_DOC_DATA.py

--- UPDATE_DOC_DATA.py
+++ UPDATE_DOC_DATA.py
@@ -5,6 +5,6 @@ content = {
     'text' : 'this is a usual text to show after title or sub title',
     'show_file' : True,
     'show_folder' : True,
-    'ignore' : ['UPDATE_DOC.py', 'UPDATE_DOC_DATA.py', 'LICENSE', 'virtual_env', '__pycache__', 'DOCUMENTATION.md', 'README.md', 'run_doc_gen_script.yml'],
-    'path' : "https://github.com/mursalatul/mini-projects/"
+    'ignore' : ['.github', '.git', 'UPDATE_DOC.py', 'UPDATE_DOC_DATA.py', 'LICENSE', 'virtual_env', '__pycache__', 'DOCUMENTATION.md', 'README.md', 'run_doc_gen_script.yml'],
+    'path' : "https://github.com/mursalatul/github-doc-gen/"
 }
